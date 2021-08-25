30e3430328560ccad7c063719b1fa22a72d37917
Lucas Lira <lucasrocha446@gmail.com>
2021-08-24 22:19:59 -0300

    feat: get the data from s3 via stream and play it on demand in sqs

--- src/s3.listener.js
+++ src/s3.listener.js
@@ -1,5 +1,6 @@
-const AWS = require('aws-sdk');
-
+const AWS = require('aws-sdk')
+const { Writable, pipeline } = require('stream')
+const csvtojson = require('csvtojson')
 class Handler {
   constructor({ s3Svc, sqsSvc }) {
     this.s3Svc = s3Svc
@@ -13,36 +14,99 @@ class Handler {
     const sqsPort = process.env.SQS_PORT || "4576"
     const isLocal = process.env.IS_LOCAL
     const s3endpoint = new AWS.Endpoint(
-        `http://${host}:${s3port}`
+      `http://${host}:${s3port}`
     )
     const s3config = {
-        endpoint: s3endpoint,
-        s3ForcePathStyle: true
+      endpoint: s3endpoint,
+      s3ForcePathStyle: true
     }
 
     const sqsEndpoint = new AWS.Endpoint(
-        `http://${host}:${sqsPort}`
+      `http://${host}:${sqsPort}`
     )
     const sqsConfig = {
-        endpoint: sqsEndpoint,
+      endpoint: sqsEndpoint,
     }
 
     if (!isLocal) {
-        delete s3config.endpoint
-        delete sqsConfig.endpoint
+      delete s3config.endpoint
+      delete sqsConfig.endpoint
     }
     return {
-        s3: new AWS.S3(s3config),
-        sqs: new AWS.SQS(sqsConfig)
+      s3: new AWS.S3(s3config),
+      sqs: new AWS.SQS(sqsConfig)
     }
   }
+  async getQueueUrl() {
+    const { QueueUrl } = await this.sqsSvc.getQueueUrl({
+      QueueName: this.queueName
+    }).promise()
 
+    return QueueUrl
+  }
+  processDataOnDemand(queueUrl) {
+    const writableStream = new Writable({
+      write: (chunk, encoding, done) => {
+        const item = chunk.toString()
+        console.log('sending..', item, 'at', new Date().toISOString())
+        this.sqsSvc.sendMessage({
+          QueueUrl: queueUrl,
+          MessageBody: item
+        }, done)
+      }
+    })
+    return writableStream
+
+  }
+  async pipefyStreams(...args) {
+    return new Promise((resolve, reject) => {
+      pipeline(
+        ...args,
+        error => error ? reject(error) : resolve()
+      )
+    })
+  }
   async main(event) {
-    console.log('***event', JSON.stringify(event, null, 2));
+    const [
+      {
+        s3: {
+          bucket: {
+            name
+          },
+          object: {
+            key
+          }
+        }
+      }
+    ] = event.Records
+
+    console.log('processing.', name, key)
+
     try {
+      console.log('getting queueURL...')
+      const queueUrl = await this.getQueueUrl()
+      const params = {
+        Bucket: name, Key: key
+      }
+      // this.s3Svc.getObject(params)
+      //     .createReadStream()
+      //     .pipe(csvtojson())
+      //     .pipe(this.processDataOnDemand(queueUrl))
+
+      // desta forma quem der erro primeiro para o processo
+      // não precisa pegar o on(finish) para retornar o handler
+      await this.pipefyStreams(
+        this.s3Svc
+          .getObject(params)
+          .createReadStream(),
+        csvtojson(),
+        this.processDataOnDemand(queueUrl)
+      )
+      console.log('process finished...', new Date().toISOString())
+
       return {
         statusCode: 200,
-        body: 'Hello',
+        body: 'Process finished with success!'
       }
     } catch (error) {
       console.log('***error', error.stack)
@@ -55,7 +119,7 @@ class Handler {
 }
 const { s3, sqs } = Handler.getSdks()
 const handler = new Handler({
-    sqsSvc: sqs,
-    s3Svc: s3
+  sqsSvc: sqs,
+  s3Svc: s3
 })
 module.exports = handler.main.bind(handler)
--- src/sqs.listener.js
+++ src/sqs.listener.js
@@ -1,10 +1,22 @@
 class Handler {
   async main(event) {
-    console.log('***event', JSON.stringify(event, null, 2));
+    const [{ body, messageId }] = event.Records
+    const item = JSON.parse(body)
+    console.log('***event',
+      JSON.stringify(
+        {
+          ...item,
+          messageId,
+          at: new Date().toISOString()
+        },
+        null,
+        2
+      )
+    )
     try {
       return {
         statusCode: 200,
-        body: 'Hello',
+        body: 'Hello'
       }
     } catch (error) {
       console.log('***error', error.stack)
